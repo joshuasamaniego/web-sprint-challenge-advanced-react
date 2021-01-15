99d99a8a1bdbb81546cca14c8def80e1fd0ee070
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2021-01-15 10:13:34 -0700

    completes Plant List section of the sprint

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,30 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
+
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const messageInput = screen.getByLabelText(/message/i);
+
+    //  2. Type values into our input fields.
+    userEvent.type(firstNameInput, "Joe");
+    userEvent.type(lastNameInput, "Smith");
+    userEvent.type(emailInput, "joesmith@gmail.com");
+    userEvent.type(messageInput, "I am awesome!");
+
+    //  3. Push the submit button.
+    const buttonInput = screen.getByRole('button');
+    userEvent.click(buttonInput);
+    
+    //Assert:
+    // Form input is on the screen.
+    const newFirstNameInput = await screen.findByText(/joe/i);
+    const newLastNameInput = await screen.findByText(/smith/i);
+    expect(newFirstNameInput).toBeInTheDocument();
+    expect(newLastNameInput).toBeInTheDocument();
+});
 
 test("form shows success message on submit with form details", () => {});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,22 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useForm = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState((initialValue) => {
+        //function needs to check if we have a value in local storage already
+        if (window.localStorage.getItem(key)) {
+            return JSON.parse(window.localStorage.getItem(key));
+        }
+        window.localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue;
+    });
+
+    //custom setter function that updates state and sets the new value to localStorage
+    const setValueToLocal = (key, newValue) => {
+        window.localStorage.setItem(JSON.stringify(key, newValue));
+        setStoredValue(newValue);
+    }
+    return [storedValue, setValueToLocal];
+}
+
+export default useForm;
